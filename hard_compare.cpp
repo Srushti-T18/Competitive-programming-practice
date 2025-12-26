5247e8565915b8d4548e46ec8523608218d4367c
Srushti Talandage <talandagesrushti@gmail.com>
2025-12-26 23:25:55 +0530

    assiut uni cf

---
+++ hard_compare.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long A, B, C, D;
+    cin >> A >> B >> C >>D;
+    long long m = A*B*C*D;
+    cout << m%100 << endl;
+    return 0;
+}
