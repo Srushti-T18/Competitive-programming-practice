9576465b5df0776249e51bf86f9f9766680ce29b
Srushti Talandage <talandagesrushti@gmail.com>
2026-02-03 12:33:43 +0530

    striversa2z

---
+++ Move0sToEnd.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n;
+    int a[n];
+    for(int i = 0; i<n; i++){
+        cin >> a[i];
+    }
+    for(int i = 0; i<n-1; i++){
+        bool swapped = false;
+        for(int j = 0; j<n-1-i; j++){
+            if(a[j]==0)
+            swap(a[j], a[j+1]);
+            swapped = true;
+        }
+         if(!swapped)
+            break;
+    }
+    for(int i = 0; i<n; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
