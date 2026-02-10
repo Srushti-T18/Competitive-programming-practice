c6e93bcd2de267c5e72ca56d3753342b7fe5fa05
Srushti Talandage <talandagesrushti@gmail.com>
2026-02-10 19:16:32 +0530

    striversa2z

---
+++ Brute2sum.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string twosum(vector<int>&a, int target){
+    int n = a.size();
+    for(int i = 0; i<n; i++){
+        for(int j = i+1; j<n; j++){
+            if(a[i]+a[j]==target){
+                return "YES";
+            }
+        }
+    }
+    return "NO";
+}
+vector<int> indices(vector<int>&a, int target){
+    int n = a.size();
+    for(int i = 0; i<n; i++){
+        for(int j = i+1; j<n; j++){
+            if(a[i]+a[j]==target){
+                return {i, j};
+            }
+        }
+    }
+    return {-1, -1};
+}
+int main(){
+    int n, target;
+    cin >> n >> target;
+    vector<int>a(n);
+    for(int i = 0; i<n; i++){
+        cin >> a[i];
+    }
+    string ans = twosum(a, target);
+    cout << ans << endl;
+    vector<int> res = indices(a, target);
+    cout << res[0] << ", " << res[1] << endl;
+    return 0;
+}
